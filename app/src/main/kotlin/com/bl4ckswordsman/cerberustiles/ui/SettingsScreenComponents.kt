738406f717ae05675f03aadd8b72dfb83c4c7d7f
bl4ckswordsman <91825788+bl4ckswordsman@users.noreply.github.com>
2026-03-22 03:41:12 +0100

    chore: clean up unused params

--- app/src/main/kotlin/com/bl4ckswordsman/cerberustiles/ui/SettingsScreenComponents.kt
+++ app/src/main/kotlin/com/bl4ckswordsman/cerberustiles/ui/SettingsScreenComponents.kt
@@ -76,7 +76,6 @@ fun createSharedParams(navController: NavController? = null): SharedParams {
     val versionManager = remember { VersionManager() }
     val coroutineScope = rememberCoroutineScope()
     val dialogType = rememberSaveable { mutableStateOf(DialogType.NONE) }
-    val showLicensesDialog = rememberSaveable { mutableStateOf(false) }
 
 
     return SharedParams(
@@ -90,7 +89,6 @@ fun createSharedParams(navController: NavController? = null): SharedParams {
         downloadManager = downloadManager,
         versionManager = versionManager,
         sharedPreferences = sharedPreferences,
-        showLicensesDialog = showLicensesDialog,
         navController = navController
     )
 }
